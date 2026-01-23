d68eee55bcc9df432dc35207ae0719d16e73f53b
celso.saraujo <celso.saraujo@hotmail.com>
2026-01-22 22:26:22 -0300

    Início de uma história

---
+++ historiaDosSonhos/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("História dos Sonhos");
+Console.WriteLine();
+
+// Capítulo 1
+Console.WriteLine("Era uma vez um grupo de pessoas que decidiu transformar sonhos em realidade...");
