47f6ed1602cbb83a13a5e88cd04a1db32e804031
Maycon Douglas <maycondouglas9292senac@gmail.com>
2026-01-29 21:00:31 -0300

    Adicionando sonho maycon

--- historiaDosSonhos/Program.cs
+++ historiaDosSonhos/Program.cs
@@ -5,3 +5,7 @@ Console.WriteLine();
 // Capítulo 1
 Console.WriteLine("Era uma vez um grupo de pessoas que decidiu transformar sonhos em realidade...");
 Console.WriteLine("Que as pessoas consigam conviver em harmonia");
+Console.WriteLine();
+
+Console.WriteLine("Meu sonho para esse ano é conseguir chegar até o final do curso de programação, sei que nao vai ser facil porem com foco e perseverança sei que conseguirei.");
+Console.WriteLine();
